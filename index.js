39d84382087e1726638f7c1de3f5e865983a4bd4
masoud <hosseini.masoud@gmail.com>
2018-12-03 23:36:15 +0330

    salam3

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const TOKEN = '658527596:AAEkA5Vkbq_RFQgxOMz4PlffltVfuu9wGuQ'
 const bot = new Telegrambot(TOKEN,{polling:true})
 bot.on('message' , msg =>{
     bot.sendMessage(msg.chat.id, 'Hello from masoud, bot says: "Hi, ${msg.form.first_name}"')
-    bot.sendMessage(msg.chat.id,'salam' ${msg.chat.id)
+    bot.sendMessage(msg.chat.id,'salam' ${msg.chat.id))
 })
