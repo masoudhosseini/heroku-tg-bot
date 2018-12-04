d596a9fe5f393fbb0414c2dbb89f021ee5cdc5a5
masoud <hosseini.masoud@gmail.com>
2018-12-04 20:17:39 +0330

    salam8

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Telegrambot = require('node-telegram-bot-api')
 const TOKEN = '658527596:AAEkA5Vkbq_RFQgxOMz4PlffltVfuu9wGuQ'
 const bot = new Telegrambot(TOKEN,{polling:true})
+const trigger = 'kobra';
 
 
 bot.on('message', (msg) => {
@@ -10,3 +11,10 @@ bot.on('message', (msg) => {
         }
     });
 });
+
+bot.on('message', (msg) => {
+    if (msg.text.toString() === trigger) {
+         bot.sendMessage(msg.chat.id, 'Hello from masoud');
+
+        }
+    });
