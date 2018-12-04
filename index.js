c2ae3e616c0b8756061e4e9f63a3ffec45ffa0f5
masoud <hosseini.masoud@gmail.com>
2018-12-04 22:59:47 +0330

    salam23

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const trigger3 = 'location';
 bot.on('message', (msg) => {
     bot.sendMessage(msg.chat.id, '', {
         reply_markup: {
-            keyboard: [[trigger], [trigger3], [trigger2]]
+            keyboard: [[trigger], 'location', [trigger2]]
         }
     });
 });
