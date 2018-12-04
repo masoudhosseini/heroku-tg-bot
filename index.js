2c9a4a54b64611509ed67b07cc12d100fcfa8fc2
masoud <hosseini.masoud@gmail.com>
2018-12-04 21:39:44 +0330

    salam16

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const Telegrambot = require('node-telegram-bot-api')
-const TOKEN = '658527596:AAEkA5Vkbq_RFQgxOMz4PlffltVfuu9wGuQ'
-const bot = new Telegrambot(TOKEN,{polling:true})
+const Telegrambot = require('node-telegram-bot-api');
+const TOKEN = '658527596:AAEkA5Vkbq_RFQgxOMz4PlffltVfuu9wGuQ';
+const bot = new Telegrambot(TOKEN,{polling:true});
 const trigger = 'kobra';
 const trigger2 = 'masoud';
 const trigger3 = 'location';
@@ -13,6 +13,7 @@ bot.on('message', (msg) => {
         }
     });
 });
+
 
 bot.on('message', (msg) => {
     if (msg.text.toString() === trigger) {
