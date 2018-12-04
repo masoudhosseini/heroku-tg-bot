d63b94824278a038e928b60fa2babc971f612125
masoud <hosseini.masoud@gmail.com>
2018-12-04 23:06:12 +0330

    salam25

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ bot.onText(/\/start/, (msg) => {
 
     bot.sendMessage(msg.chat.id, "Welcome", {
         "reply_markup": {
-            "keyboard": [["Sample text", "Second sample"],   ["Keyboard"], ["I'm robot"]]
+            "keyboard": [["Sample text", "Second sample"],   ["Keyboard"], ["location"]]
         }
     });
 
