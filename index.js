b0f0d36d27916fbff848bd6038397cfbcb275079
masoud <hosseini.masoud@gmail.com>
2018-12-04 21:30:05 +0330

    salam14

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const TOKEN = '658527596:AAEkA5Vkbq_RFQgxOMz4PlffltVfuu9wGuQ'
 const bot = new Telegrambot(TOKEN,{polling:true})
 const trigger = 'kobra';
 const trigger2 = 'masoud';
+const trigger3 = 'location';
 
 
 bot.on('message', (msg) => {
     bot.sendMessage(msg.chat.id,  {
         reply_markup: {
-            keyboard: [['kobra'], ['masoud']]
+            keyboard: [['kobra'], ['masoud'], ['location']]
         }
     });
 });
@@ -22,5 +23,10 @@ bot.on('message', (msg) => {
 bot.on('message', (msg) => {
     if (msg.text.toString() === trigger2) {
         bot.sendPhoto(msg.chat.id,photo="http://pilatesandyogafitness.com/wp-content/uploads/2018/01/Heart-clip-art-clipartandscrap.png");
+    }
+});
+bot.on('message', (msg) => {
+    if (msg.text.toString() === trigger3) {
+        bot.sendLocation(msg.chat.id,35.7797061 ,51.3487489)
     }
 });
