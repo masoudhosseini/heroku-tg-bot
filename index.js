f437f680b1f45abc5fb1f14506fbfd3dd77709b9
masoud <hosseini.masoud@gmail.com>
2018-12-05 21:29:06 +0330

    salam30

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const bot = new Telegrambot(TOKEN,{polling:true})
 const trigger = 'kobra'
 const trigger2 = 'masoud'
 const trigger3 = 'location'
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 bot.onText(/\/start/, (msg) => {
 
     bot.sendMessage(msg.chat.id, "Welcome", {
