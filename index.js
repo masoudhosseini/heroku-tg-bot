2e13217e6535d30bd93f579a952afa6bbfe07655
masoud <hosseini.masoud@gmail.com>
2018-12-06 01:36:07 +0330

    salam49

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Telegrambot = require('node-telegram-bot-api')
 const TOKEN = '658527596:AAEkA5Vkbq_RFQgxOMz4PlffltVfuu9wGuQ'
 const bot = new Telegrambot(TOKEN,{polling:true})
-const trigger = 'kobra'
+const trigger = 'دما'
 const trigger2 = 'masoud'
 const trigger3 = 'location'
 var express = require('express');
@@ -9,7 +9,7 @@ var port = process.env.PORT || 3000;
 var app = express();
 let request = require('request');
 let apiKey = '6ee20a29bb9dd0705999f0d8529b3224';
-let url = 'http://api.openweathermap.org/data/2.5/weather?q=Orumiyeh,ir&APPID=6ee20a29bb9dd0705999f0d8529b3224'
+let url = 'http://api.openweathermap.org/data/2.5/weather?q=Orumiyeh,ir&units=metric&APPID=6ee20a29bb9dd0705999f0d8529b3224'
 app.get('/', function (req, res) {
 res.send(JSON.stringify({ Hello: 'World'}));
 });
@@ -20,7 +20,7 @@ bot.onText(/\/start/, (msg) => {
 
     bot.sendMessage(msg.chat.id, "Welcome", {
         "reply_markup": {
-            "keyboard": [["masoud", "Second sample"],   ["kobra"], ["location"]]
+            "keyboard": [["masoud", "Second sample"],   ["دما"], ["location"]]
         }
     });
 
@@ -29,7 +29,6 @@ bot.onText(/\/start/, (msg) => {
 
 bot.on('message', (msg) => {
     if (msg.text.toString() === trigger) {
-         bot.sendMessage(msg.chat.id, 'Hello from masoud');
         request(url, function (err, response, body) {
             if(err){
                 console.log('error:', error);
