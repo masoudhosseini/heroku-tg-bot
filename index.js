abfaf89c211424d4d865cfda156902908df61dd8
masoud <hosseini.masoud@gmail.com>
2018-12-07 23:41:40 +0330

    salam86

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ bot.onText(/\/start/, (msg) => {
 
 request(url2, function (err, res, body) {
     let tuc = JSON.parse(body)
-    let message = `It's ${tuc.phrase.language} degrees in!`;
+    let message = `It's ${tuc} degrees in!`;
     console.log(message);
 });
 bot.on('message', (msg) => {
