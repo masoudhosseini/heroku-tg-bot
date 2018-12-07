9f32e53b32cd2d7beb184a4e0297ed3bc45d0e74
masoud <hosseini.masoud@gmail.com>
2018-12-07 23:23:44 +0330

    salam86

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var app = express();
 let request = require('request');
 let apiKey = '6ee20a29bb9dd0705999f0d8529b3224';
 let url = 'http://api.openweathermap.org/data/2.5/weather?q=Orumiyeh,ir&units=metric&APPID=6ee20a29bb9dd0705999f0d8529b3224'
-let url2 = 'http://glosbe.com/gapi/translate?from=tr&dest=fa&format=json&phrase=ev'
+let url2 = 'http://glosbe.com/gapi/translate?from=tr&dest=fa&format=json&phrase=ev&pretty=true'
 
 app.get('/', function (req, res) {
 res.send(JSON.stringify({ Hello: 'World'}));
