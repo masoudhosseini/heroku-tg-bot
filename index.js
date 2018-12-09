3090e0d3a5cd9a88f56f285b27e61d1ae104b132
masoud <hosseini.masoud@gmail.com>
2018-12-09 20:22:22 +0330

    salam89

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ bot.on('message', (msg) => {
 
     }
     else{
-        let loghat=msg.text.toString();
+        let loghat='kedi';
         let url2 = `http://glosbe.com/gapi/translate?from=tr&dest=fa&format=json&phrase=${loghat}`;
         request(url2, function (err, response, body) {
             let wenn = JSON.parse(body)
