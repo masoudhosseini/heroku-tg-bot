4d5a33cd43895b7a86a29d1c3663b608a53307b9
masoud <hosseini.masoud@gmail.com>
2018-12-09 23:48:22 +0330

    salam89

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ bot.on('message', (msg) => {
     else {
          let loghat = msg.text;
 
-            let url2 = `http://glosbe.com/gapi/translate?from=${fromvar1}&dest=${tovar1}&format=json&phrase=${loghat}`;
+            let url2 = `http://glosbe.com/gapi/translate?from=tr&dest=fa&format=json&phrase=${loghat}`;
 
          bot.sendMessage(msg.chat.id, url2);
 
