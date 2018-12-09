44dc4b3d1e566a51d1a00783b6642228e8c32ca3
masoud <hosseini.masoud@gmail.com>
2018-12-09 23:43:35 +0330

    salam89

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ var app = express();
 let request = require('request');
 let apiKey = '6ee20a29bb9dd0705999f0d8529b3224';
 let url = 'http://api.openweathermap.org/data/2.5/weather?q=Orumiyeh,ir&units=metric&APPID=6ee20a29bb9dd0705999f0d8529b3224'
-let tovar1='';
-let fromvar1='';
+let tovar1='fa';
+let fromvar1='tr';
 
 app.get('/', function (req, res) {
     res.send(JSON.stringify({ Hello: 'World'}));
@@ -51,12 +51,14 @@ bot.on('message', (msg) => {
 
          fromvar1='tr';
          tovar1='fa';
+         bot.sendMessage(msg.chat.id, fromvar1);
 
     }
     else if (msg.text.toString() === trigger4) {
 
          fromvar1='fa';
           tovar1='tr';
+         bot.sendMessage(msg.chat.id, fromvar1);
 
     }
     else {
