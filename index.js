ab20e4b78c3a9973a21f9d64d5a6b5295c60eaa0
masoud <hosseini.masoud@gmail.com>
2018-12-10 00:15:38 +0330

    salam90

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ let request = require('request');
 let apiKey = '6ee20a29bb9dd0705999f0d8529b3224';
 let url = 'http://api.openweathermap.org/data/2.5/weather?q=Orumiyeh,ir&units=metric&APPID=6ee20a29bb9dd0705999f0d8529b3224'
 
-
 app.get('/', function (req, res) {
     res.send(JSON.stringify({ Hello: 'World'}));
 });
@@ -46,19 +45,14 @@ bot.on('message', (msg) => {
 
 
     }
-    else if (msg.text.toString() === trigger2) {
-
-
-
-    }
-    else if (msg.text.toString() === trigger4) {
-
-
-
-    }
     else {
          let loghat = msg.text;
-            let url2 = `http://glosbe.com/gapi/translate?from=tr&dest=fa&format=json&phrase=${loghat}`;
+
+         let url2 = `http://glosbe.com/gapi/translate?from=tr&dest=fa&format=json&phrase=${loghat}`;
+
+
+         console.log(url2);
+
          request(url2, function (err, response, body) {
                 let wenn = JSON.parse(body);
                 if (wenn.tuc.length < 1) {
