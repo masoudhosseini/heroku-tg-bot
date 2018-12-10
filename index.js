c1d96527143796f97b87113ac0302d2a8ff22702
masoud <hosseini.masoud@gmail.com>
2018-12-10 22:09:47 +0330

    salam100

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ bot.on('message', (msg) => {
 
     }
     else {
-        let loghat=msg.text;
+        let loghat=msg.text.toLowerCase();
         let url2 = `http://glosbe.com/gapi/translate?from=tr&dest=fa&format=json&phrase=${loghat}`;
         request(url2, function (err, response, body) {
             if(err){
