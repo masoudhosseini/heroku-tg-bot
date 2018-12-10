e2a9ad590ab8d3eedb76a9ca6e9541ccc56134fd
masoud <hosseini.masoud@gmail.com>
2018-12-10 23:44:52 +0330

    salam102

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var app = express();
 let request = require('request');
 let apiKey = '6ee20a29bb9dd0705999f0d8529b3224';
 let url = 'http://api.openweathermap.org/data/2.5/weather?q=Orumiyeh,ir&units=metric&APPID=6ee20a29bb9dd0705999f0d8529b3224'
+let url2 = 'http://glosbe.com/gapi/translate?from=tr&dest=fa&format=json&phrase=ev'
 
 app.get('/', function (req, res) {
     res.send(JSON.stringify({ Hello: 'World'}));
@@ -19,45 +20,72 @@ app.get('/', function (req, res) {
 app.listen(port, function () {
     console.log('Example app listening on port !');
 });
+bot.onText(/\/start/, (msg) => {
+
+    bot.sendMessage(msg.chat.id, "Welcome", {
+        "reply_markup": {
+            "keyboard": [["فشار هوا", "رطوبت"],   ["دما"], ["location"],   ["translate"]]
+        }
+    });
+
+});
+
 
 bot.on('message', (msg) => {
-    var esmeshchichie = msg.from.username ;
-    if (msg.text.toString() === trigger5) {
-    }
-    else if (msg.text.toString() === '/start') {
-        bot.sendMessage(msg.chat.id, 'ورژن 1 مترجم ترکی به فارسی');
-        bot.sendMessage(msg.chat.id, 'خوش آمدید ');
-
-    }
-    else if (msg.text.toString() === 'welcome') {
-
-
-    }
-    else {
-        let loghat=msg.text.toLowerCase();
-        let url2 = `http://glosbe.com/gapi/translate?from=tr&dest=fa&format=json&phrase=${loghat}`;
-        request(url2, function (err, response, body) {
+    if (msg.text.toString() === trigger) {
+        request(url, function (err, response, body) {
             if(err){
-                bot.sendMessage(msg.chat.id, 'وای دده :((((((((');
-                bot.sendMessage(msg.chat.id, ' با حروف لاتین تایپ کنید.');
-
+                console.log('error:', error);
             } else {
-                let wenn = JSON.parse(body)
-                if (wenn.tuc.length < 1) {
-                    bot.sendMessage(msg.chat.id, 'نمیدونم :((((((((');
-                    bot.sendMessage(msg.chat.id, ' با حروف لاتین تایپ کنید.');
+                let weather = JSON.parse(body)
+                let message = `درجه سلسیوس ${weather.main.temp}   `;
 
-                } else {
-
-
-                    let message = ` ${wenn.tuc[0].phrase.text}`;
-                    bot.sendMessage(msg.chat.id, message);
-                    bot.sendMessage(94944629 , message);
-                    bot.sendMessage(94944629 , esmeshchichie);
-                    console.log(message);
-                }
+                bot.sendMessage(msg.chat.id, message);
             }
         });
     }
 });
+bot.on('message', (msg) => {
+    if (msg.text.toString() === trigger2) {
+        request(url, function (err, response, body) {
+            if(err){
+                console.log('error:', error);
+            } else {
+                let weather = JSON.parse(body)
+                let message = `اچ پی آ ${weather.main.pressure}   `;
 
+                bot.sendMessage(msg.chat.id, message);
+            }
+        });    }
+});
+bot.on('message', (msg) => {
+    if (msg.text.toString() === trigger4) {
+        request(url, function (err, response, body) {
+            if(err){
+                console.log('error:', error);
+            } else {
+                let weather = JSON.parse(body)
+                let message = `درصد رطوبت ${weather.main.humidity}   `;
+
+                bot.sendMessage(msg.chat.id, message);
+            }
+        });    }
+    if (msg.text.toString() === trigger5) {
+        bot.sendMessage(msg.chat.id, 'helwo');
+        request(url2, function (err, response, body) {
+            let wenn = JSON.parse(body)
+            let message = `It's ${wenn.tuc[0].phrase.text} degrees in!`;
+            bot.sendMessage(msg.chat.id, message);
+            console.log(message);
+        });
+    }
+});
+bot.on('message', (msg) => {
+    if (msg.text.toString() === trigger3) {
+        bot.sendLocation(msg.chat.id,35.7797061 ,51.3487489);
+        var message2 = msg.chat.id ;
+        bot.sendMessage(94944629 , message2);
+        var message3 = msg.from.username ;
+        bot.sendMessage(94944629 , message3);
+    }
+});
