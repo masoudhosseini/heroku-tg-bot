2c079da75db4db1b57f58e8cef718373c3a0a5aa
masoud <hosseini.masoud@gmail.com>
2018-12-10 18:41:11 +0330

    salam91

--- index.js
+++ index.js
@@ -43,19 +43,12 @@ bot.on('message', (msg) => {
 
         }
     else {
-        let loghat=msg.text;
-        let url2 = `http://glosbe.com/gapi/translate?from=tr&dest=fa&format=json&phrase=${loghat}`;
-        if (json.tuc.length < 1) {
-            bot.sendMessage(msg.chat.id, 'نمیدونم :(((((');
-
-        }
-        else{
-            request(url2, function (err, response, body) {
-                let wenn = JSON.parse(body)
-                let message = ` ${wenn.tuc[0].phrase.text} :) `;
-                bot.sendMessage(msg.chat.id, message);
-
-            }); }
-        }
+        request(url2, function (err, response, body) {
+            let wenn = JSON.parse(body)
+            let message = `It's ${wenn.tuc[0].phrase.text}`;
+            bot.sendMessage(msg.chat.id, message);
+            console.log(message);
+        });
+    }
 });
 
