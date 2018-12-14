805b1e480553ba406c17eaa6af1376f47943e155
masoud <hosseini.masoud@gmail.com>
2018-12-14 17:05:09 +0330

    salam106

--- index.js
+++ index.js
@@ -22,7 +22,29 @@ app.get('/', function (req, res) {
 app.listen(port, function () {
     console.log('Example app listening on port !');
 });
+// Adds the utterances and intents for the NLP
+manager.addDocument('en', 'goodbye for now', 'greetings.bye');
+manager.addDocument('en', 'bye bye take care', 'greetings.bye');
+manager.addDocument('en', 'okay see you later', 'greetings.bye');
+manager.addDocument('en', 'bye for now', 'greetings.bye');
+manager.addDocument('en', 'i must go', 'greetings.bye');
+manager.addDocument('en', 'hello', 'greetings.hello');
+manager.addDocument('en', 'hi', 'greetings.hello');
+manager.addDocument('en', 'howdy', 'greetings.hello');
 
+// Train also the NLG
+manager.addAnswer('en', 'greetings.bye', 'Till next time');
+manager.addAnswer('en', 'greetings.bye', 'see you soon!');
+manager.addAnswer('en', 'greetings.hello', 'Hey there!');
+manager.addAnswer('en', 'greetings.hello', 'Greetings!');
+
+// Train and save the model.
+(async() => {
+    await manager.train();
+    manager.save();
+    responsex = await manager.process('en', 'I have to go');
+    console.log(responsex);
+})();
 bot.on('message', (msg) => {
     bot.sendMessage(msg.chat.id, responsex);
     var esmeshchichie = msg.from.username ;
@@ -64,26 +86,4 @@ bot.on('message', (msg) => {
         });
     }
 });
-// Adds the utterances and intents for the NLP
-manager.addDocument('en', 'goodbye for now', 'greetings.bye');
-manager.addDocument('en', 'bye bye take care', 'greetings.bye');
-manager.addDocument('en', 'okay see you later', 'greetings.bye');
-manager.addDocument('en', 'bye for now', 'greetings.bye');
-manager.addDocument('en', 'i must go', 'greetings.bye');
-manager.addDocument('en', 'hello', 'greetings.hello');
-manager.addDocument('en', 'hi', 'greetings.hello');
-manager.addDocument('en', 'howdy', 'greetings.hello');
 
-// Train also the NLG
-manager.addAnswer('en', 'greetings.bye', 'Till next time');
-manager.addAnswer('en', 'greetings.bye', 'see you soon!');
-manager.addAnswer('en', 'greetings.hello', 'Hey there!');
-manager.addAnswer('en', 'greetings.hello', 'Greetings!');
-
-// Train and save the model.
-(async() => {
-    await manager.train();
-    manager.save();
-     responsex = await manager.process('en', 'I have to go');
-    console.log(responsex);
-})();
