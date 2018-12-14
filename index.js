958f5ef3f0d0e753a8f3df841fb74ad4c20efac8
masoud <hosseini.masoud@gmail.com>
2018-12-14 16:30:39 +0330

    salam104

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ var app = express();
 let request = require('request');
 let apiKey = '6ee20a29bb9dd0705999f0d8529b3224';
 let url = 'http://api.openweathermap.org/data/2.5/weather?q=Orumiyeh,ir&units=metric&APPID=6ee20a29bb9dd0705999f0d8529b3224'
+const { NlpManager } = require('node-nlp');
+const manager = new NlpManager({ languages: ['en'] });
 
 app.get('/', function (req, res) {
     res.send(JSON.stringify({ Hello: 'World'}));
@@ -35,8 +37,7 @@ bot.on('message', (msg) => {
     }
     else {
         let loghat=msg.text.toLowerCase();
-        loghat.setEncoding('utf8');
-        let url2 = `http://glosbe.com/gapi/translate?from=fa&dest=tr&format=json&phrase=${loghat}`;
+        let url2 = `http://glosbe.com/gapi/translate?from=tr&dest=fa&format=json&phrase=${loghat}`;
         request(url2, function (err, response, body) {
             if(err){
                 bot.sendMessage(msg.chat.id, 'وا? دده :((((((((');
@@ -61,4 +62,26 @@ bot.on('message', (msg) => {
         });
     }
 });
+// Adds the utterances and intents for the NLP
+manager.addDocument('en', 'goodbye for now', 'greetings.bye');
+manager.addDocument('en', 'bye bye take care', 'greetings.bye');
+manager.addDocument('en', 'okay see you later', 'greetings.bye');
+manager.addDocument('en', 'bye for now', 'greetings.bye');
+manager.addDocument('en', 'i must go', 'greetings.bye');
+manager.addDocument('en', 'hello', 'greetings.hello');
+manager.addDocument('en', 'hi', 'greetings.hello');
+manager.addDocument('en', 'howdy', 'greetings.hello');
 
+// Train also the NLG
+manager.addAnswer('en', 'greetings.bye', 'Till next time');
+manager.addAnswer('en', 'greetings.bye', 'see you soon!');
+manager.addAnswer('en', 'greetings.hello', 'Hey there!');
+manager.addAnswer('en', 'greetings.hello', 'Greetings!');
+
+// Train and save the model.
+(async() => {
+    await manager.train();
+    manager.save();
+    const response = await manager.process('en', 'I have to go');
+    console.log(response);
+})();
