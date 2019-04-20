e5c13d36527a8d5a419e1891dc08fa0bce2ec16e
masoud <hosseini.masoud@gmail.com>
2019-04-20 10:55:35 +0430

    salam110

--- index.js
+++ index.js
@@ -1,20 +1,4 @@
-function sayhelloo(name) {
-    console.log('helloo'+name);
-}
-sayhelloo('masoudjan');
 
-var logger = require('./logger');
-logger.sih('hasan');
-const path = require('path');
-var pathobj = path.parse(__filename);
-console.log(pathobj);
-const os = require('os');
-var totalmemm =os.totalmem();
-var freememm =os.freemem();
-
-console.log('totalmemory ' + totalmemm);
-console.log(freememm);
-console.log(`total memory: ${totalmemm}`);
 const DigikeyParser = require('./digikey_parser2');
 
 DigikeyParser('5027543', function(data) {
